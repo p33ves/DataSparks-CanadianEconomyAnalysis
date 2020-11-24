73566765e1c84ad44f92915fb570f69ebebe4dff
Vignesh P <vignesh.p1110@gmail.com>
2020-11-24 21:40:48 +0530

    Fixing download file type

--- src/get_statcan_data.py
+++ src/get_statcan_data.py
@@ -2,8 +2,8 @@ import requests
 import os
 from stats_can import scwds
 
-#print(os.getcwd())
-os.chdir("..")      #Shift back to project repo
+# print(os.getcwd())
+os.chdir("..")      # Shift back to project repo
 
 table_list = open('statcan_url_list.txt', 'r')
 for line in table_list:
@@ -13,8 +13,8 @@ for line in table_list:
     with requests.get(url, stream=True) as res:
         res.raise_for_status()
         print(f"Downloading - {title}")
-        with open(f'data/{title}.csv', 'wb') as target:
+        with open(f'data/{title}.zip', 'wb') as target:
             for chunk in res.iter_content(chunk_size=8192):
                 if chunk:
                     target.write(chunk)
-        print(f"{title} download complete!")
+        print(f"Download complete - {title} ")
