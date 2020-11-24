840f05851cf10d392ded87df82343179e9989912
Vignesh P <vignesh.p1110@gmail.com>
2020-11-24 21:22:28 +0530

    Fetching statcan data

---
+++ src/get_statcan_data.py
@@ -0,0 +1,20 @@
+import requests
+import os
+from stats_can import scwds
+
+#print(os.getcwd())
+os.chdir("..")      #Shift back to project repo
+
+table_list = open('statcan_url_list.txt', 'r')
+for line in table_list:
+    title = line.split(':')[0].strip()
+    table = line.split(':')[1].strip()
+    url = scwds.get_full_table_download(table)
+    with requests.get(url, stream=True) as res:
+        res.raise_for_status()
+        print(f"Downloading - {title}")
+        with open(f'data/{title}.csv', 'wb') as target:
+            for chunk in res.iter_content(chunk_size=8192):
+                if chunk:
+                    target.write(chunk)
+        print(f"{title} download complete!")
