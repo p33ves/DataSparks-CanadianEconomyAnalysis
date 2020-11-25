b99b4c95645a513a3ff3ca5724314d8dabc769ec
Vignesh P <vignesh.p1110@gmail.com>
2020-11-25 22:01:09 +0530

    outpath directory modified, extension fixed

--- src/get_statcan_data.py
+++ src/get_statcan_data.py
@@ -2,19 +2,22 @@ import requests
 import os
 from stats_can import scwds
 
-# print(os.getcwd())
+print(os.getcwd())
 os.chdir("..")      # Shift back to project repo
+OUTPATH = "./data/raw/statcan/"
+os.makedirs(OUTPATH, exist_ok=True)
 
 table_list = open('statcan_url_list.txt', 'r')
 for line in table_list:
     title = line.split(':')[0].strip()
     table = line.split(':')[1].strip()
-    url = scwds.get_full_table_download(table)
-    with requests.get(url, stream=True) as res:
+    url_path = scwds.get_full_table_download(table)
+    _, ext = os.path.splitext(url_path)
+    file_name = os.path.join(OUTPATH, title + ext)
+    with requests.get(url_path, stream=True) as res, open(file_name, 'wb') as out_file:
         res.raise_for_status()
         print(f"Downloading - {title}")
-        with open(f'data/{title}.zip', 'wb') as target:
-            for chunk in res.iter_content(chunk_size=8192):
-                if chunk:
-                    target.write(chunk)
+        for chunk in res.iter_content(chunk_size=8192):
+            if chunk:
+                out_file.write(chunk)
         print(f"Download complete - {title} ")
