8885c6e3618185e700489b671f46b55c9d40ba4a
Vignesh P <vignesh.p1110@gmail.com>
2020-11-27 14:43:07 +0530

    Added skip condition to extraction

--- src/get_statcan_data.py
+++ src/get_statcan_data.py
@@ -6,8 +6,8 @@ import zipfile
 from multiprocessing import Pool, cpu_count
 from stats_can import scwds
 
-OUTPATH = "../data/raw/statcan/"
-os.makedirs(OUTPATH, exist_ok=True)
+OUT_PATH = "../data/raw/statcan/"
+os.makedirs(OUT_PATH, exist_ok=True)
 
 
 def download_zips(line):
@@ -15,22 +15,27 @@ def download_zips(line):
     table_id = line.split(':')[1].strip()
     url_path = scwds.get_full_table_download(table_id)
     _, ext = os.path.splitext(url_path)
-    download_file = os.path.join(OUTPATH, title + ext)
+    download_file = os.path.join(OUT_PATH, title + ext)
     if os.path.exists(download_file):
         print(f"Download skipped - {title} ")
-    with requests.get(url_path, stream=True) as response, open(download_file, 'wb') as out_file:
-        response.raise_for_status()
-        print(f"Downloading - {title}")
-        for chunk in response.iter_content(chunk_size=8192):
-            if chunk:
-                out_file.write(chunk)
-        print(f"Download complete - {title}")
+    else:
+        with requests.get(url_path, stream=True) as response, open(download_file, 'wb') as out_file:
+            response.raise_for_status()
+            print(f"Downloading - {title}")
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:
+                    out_file.write(chunk)
+            print(f"Download complete - {title}")
     input_zip = zipfile.ZipFile(download_file)
     input_file = input_zip.namelist()[0]
-    input_zip.extract(input_file, OUTPATH)
-    print(f"Extracted {input_file} for - {title} ")
+    if os.path.exists(OUT_PATH + input_file):
+        print(f"Extraction skipped - {title} ")
+    else:
+        input_zip.extract(input_file, OUT_PATH)
+        print(f"Extracted {input_file} for - {title} ")
     input_zip.close()
     return {table_id: title}
+
 
 if __name__ == "__main__":
     print("There are {} CPUs on this machine ".format(cpu_count()))
@@ -39,3 +44,4 @@ if __name__ == "__main__":
     results = pool.map(download_zips, table_list)
     pool.close()
     pool.join()
+    print("Stat_can files used in the project are : ", results)
