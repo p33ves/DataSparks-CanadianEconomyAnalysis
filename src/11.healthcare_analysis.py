49a2142919e2f37596a71be070a0a5c6ac8ee90d
Vignesh P <vignesh.p1110@gmail.com>
2020-12-08 19:22:38 +0530

    added imt import/export data for pharma products

--- src/11.healthcare_analysis.py
+++ src/11.healthcare_analysis.py
@@ -2,23 +2,22 @@ import os
 import json
 import boto3
 
-
 from pyspark.sql import SparkSession, types, functions, column
 from pyspark.sql.functions import to_date, avg
 
 IN_PATH = "s3://mysparks/data/clean/statcan/"
 OUT_PATH = "s3://mysparks/OUTPUT-Folder/"
-#IN_PATH = "../data/clean/statcan/"
-#OUT_PATH = "../OUTPUT-Folder/"
-
 SCHEMA_PATH = "schema/statcan/"
+
 # IN_PATH = "../data/clean/statcan/"
 # OUT_PATH = "../OUTPUT-Folder/"
 # SCHEMA_PATH = "../schema/statcan/"
+
 gdp_id = "36100434"
 cpi_id = "18100004"
 cc_id = "13100781"
 retail_id = "20100008"
+imt_id = "12100121"
 
 s3_obj = boto3.client('s3')
 s3_gdp_obj = s3_obj.get_object(Bucket='mysparks', Key=SCHEMA_PATH + "gdp.json")
@@ -37,17 +36,18 @@ s3_retail_obj = s3_obj.get_object(Bucket='mysparks', Key=SCHEMA_PATH + "retailsa
 s3_retail_data = s3_retail_obj['Body'].read().decode('utf-8')
 retail_schema = json.loads(s3_retail_data)
 
+s3_imt_obj = s3_obj.get_object(Bucket='mysparks', Key=SCHEMA_PATH + "mt.json")
+s3_imt_data = s3_imt_obj['Body'].read().decode('utf-8')
+imt_schema = json.loads(s3_imt_data)
+
 # gdp_schema = json.load(open(SCHEMA_PATH + "gdp.json"))
 # cpi_schema = json.load(open(SCHEMA_PATH + cpi_id + ".json"))
 # cc_schema = json.load(open(SCHEMA_PATH + "covid_cases.json"))
 # retail_schema = json.load(open(SCHEMA_PATH + "retailsales_canada.json"))
+# imt_schema = json.load(open(SCHEMA_PATH + "mt.json"))
 
 os.makedirs(OUT_PATH, exist_ok=True)
 
-#gdp_schema = json.load(open("../schema/statcan/" + gdp_id + ".json"))
-#cpi_schema = json.load(open("../schema/statcan/" + cpi_id + ".json"))
-#cc_schema = json.load(open("../schema/statcan/" + cc_id + ".json"))
-#retail_schema = json.load(open("../schema/statcan/" + retail_id + ".json"))
 
 def main():
     gdp = spark.read.csv(OUT_PATH + 'GDP_output/*.csv',
@@ -58,9 +58,12 @@ def main():
                                  schema=types.StructType.fromJson(cc_schema)).cache()  # reading Covid Cases Data csv
     retail = spark.read.csv(OUT_PATH + 'Retail2_output/*.csv',
                             schema=types.StructType.fromJson(retail_schema))  # reading retail Data csv
+    imt = spark.read.csv(OUT_PATH + 'MT_output/*.csv',
+                         schema=types.StructType.fromJson(imt_schema))  # reading GDP Data csv
 
     # region GDP data selection
-    select_fields = gdp.filter((functions.year(gdp['REF_DATE']) == 2020) & (gdp['NAICS'].contains('care')))
+    select_fields = gdp.filter(((functions.year(gdp['REF_DATE']) == 2020) | (functions.year(gdp['REF_DATE']) == 2019))
+                               & (gdp['NAICS'].contains('care')))
     care_fields = select_fields.withColumn('Temp', functions.concat(
         functions.lit("GDP of "), select_fields['NAICS'], functions.lit(" x10^6 (in CAD)")))
     healthcare_gdp = care_fields.groupby(care_fields['REF_DATE']) \
@@ -68,8 +71,8 @@ def main():
     # endregion
 
     # region CPI data selection
-    select_fields = cpi.filter((functions.year(cpi['REF_DATE']) == 2020) & (cpi['GEO'] == "Canada") &
-                               ((cpi['Products and product groups'].contains("health")) |
+    select_fields = cpi.filter(((functions.year(cpi['REF_DATE']) == 2020) | (functions.year(cpi['REF_DATE']) == 2019))
+                               & (cpi['GEO'] == "Canada") & ((cpi['Products and product groups'].contains("health")) |
                                 (cpi['Products and product groups'] == "Medicinal and pharmaceutical products")))
     health_fields = select_fields.withColumn('Temp', functions.concat(
         functions.lit("CPI of "), select_fields['Products and product groups']))
@@ -78,38 +81,67 @@ def main():
     # endregion
 
     # region Retail data select
-    healthcare_retail = retail.filter((functions.year(retail['REF_DATE']) == 2020)) \
+    healthcare_retail = retail.filter((functions.year(retail['REF_DATE']) == 2020) |
+                                      (functions.year(retail['REF_DATE']) == 2019)) \
         .select(retail['REF_DATE'], retail["Health and personal care stores [446]"]
                 .alias("Retail Trade sales for - Health and personal care stores x10^3 (in CAD)"))
     # endregion
 
-    healthcare_data = healthcare_gdp.join(healthcare_cpi, healthcare_gdp['REF_DATE'] == healthcare_cpi['REF_DATE'])\
-        .join(healthcare_retail, healthcare_gdp['REF_DATE'] == healthcare_retail['REF_DATE'])\
-        .drop(healthcare_cpi['REF_DATE']).drop(healthcare_retail['REF_DATE'])
+    # region IMT data selection
+    reqd_rows = imt.filter(((functions.year(imt['YEAR']) == 2020) | (functions.year(imt['YEAR']) == 2019))
+                           & (imt['Basis'] == "Balance of payments")
+                           & (imt['NAPCS'] == "Pharmaceutical and medicinal products [273]"))
+    import_rows = reqd_rows.filter(imt['Trade'] == "Import") \
+        .select(reqd_rows['YEAR'], reqd_rows['Total Merch Trade Value'].alias("Int. Merchandise Trade for "
+                                                                              "Pharma&Medical products -Imports x10^6 "
+                                                                              "(in CAD)"))
+    export_rows = reqd_rows.filter(imt['Trade'] == "Export"). \
+        select(reqd_rows['YEAR'].alias("Ex_year"),
+               reqd_rows['Total Merch Trade Value'].alias("Int. Merchandise Trade for Pharma&Medical products "
+                                                          "-Exports x10^6 (in CAD)"))
+
+    healthcare_imt = import_rows.join(export_rows, import_rows['YEAR'] == export_rows['Ex_year'])\
+        .drop(export_rows['Ex_year'])
+    # endregion
+
+    healthcare_data = healthcare_gdp.join(healthcare_cpi, healthcare_gdp['REF_DATE'] == healthcare_cpi['REF_DATE']) \
+        .join(healthcare_retail, healthcare_gdp['REF_DATE'] == healthcare_retail['REF_DATE']) \
+        .join(healthcare_imt, healthcare_gdp['REF_DATE'] == healthcare_imt['YEAR']) \
+        .drop(healthcare_cpi['REF_DATE']).drop(healthcare_retail['REF_DATE']).drop(healthcare_imt['YEAR'])
 
     # region covid_cases
     monthly_cases = covid_cases.groupby(covid_cases['Episode month'])
-    new_cases = monthly_cases.count()\
+    new_cases = monthly_cases.count() \
         .withColumnRenamed('Episode month', 'Month').withColumnRenamed('count', 'New cases')
-    recovered_cases = covid_cases.filter(covid_cases['Recovered']).groupby(covid_cases['Recovery month']).count()\
+    recovered_cases = covid_cases.filter(covid_cases['Recovered']).groupby(covid_cases['Recovery month']).count() \
         .withColumnRenamed('count', 'Recovered cases')
-    deaths = covid_cases.filter(covid_cases['Death']).groupby(covid_cases['Episode month']).count()\
+    deaths = covid_cases.filter(covid_cases['Death']).groupby(covid_cases['Episode month']).count() \
         .withColumnRenamed('count', 'Deaths')
+    healthcare_worker_count = covid_cases.filter(covid_cases['Occupation'] == "Health care worker")\
+        .groupby(covid_cases['Episode month']).count()\
+        .withColumnRenamed('Episode month', 'For Month').withColumnRenamed('count', 'Healthcare workers affected')
     hospital_statuses = monthly_cases.pivot('Hospital status',
-                                                  ["Hospitalized and in ICU", "Hospitalized, but not in ICU",
-                                                   "Not hospitalized", "Not Stated/Unknown"]).count()
-    covid_trend = new_cases.join(recovered_cases, new_cases['Month'] == recovered_cases['Recovery month'])\
-        .join(deaths, new_cases['Month'] == deaths['Episode month']) \
+                                            ["Hospitalized and in ICU", "Hospitalized, but not in ICU",
+                                             "Not hospitalized", "Not Stated/Unknown"]).count()
+
+    covid_trend = new_cases.join(recovered_cases, new_cases['Month'] == recovered_cases['Recovery month'], 'left') \
+        .join(deaths, new_cases['Month'] == deaths['Episode month'], 'left') \
         .drop(recovered_cases['Recovery month']).drop(deaths['Episode month']) \
-        .join(hospital_statuses, new_cases['Month'] == hospital_statuses['Episode month'])\
-        .drop(hospital_statuses['Episode month'])
+        .join(hospital_statuses, new_cases['Month'] == hospital_statuses['Episode month']) \
+        .drop(hospital_statuses['Episode month'])\
+        .join(healthcare_worker_count, new_cases['Month'] == healthcare_worker_count['For Month'], 'left') \
+        .drop(healthcare_worker_count['For Month'])
     # endregion
 
     # region final_df -> REF_DATE, healthcare_data + covid_trend
     final_df = covid_trend.join(healthcare_data, covid_trend['Month'] == healthcare_data['REF_DATE'], 'outer')
+    final_df = final_df.withColumn('ProperMonth', to_date(final_df['Month'], 'yyyy-MM')) \
+        .drop(final_df['Month'])
+    final_df = final_df.withColumn('Month', functions.coalesce(final_df['ProperMonth'], final_df['REF_DATE'])) \
+        .drop(final_df['ProperMonth']).drop(final_df['REF_DATE'])
     with open(SCHEMA_PATH + "healthcare_analysis.json", 'w') as out_file:
         out_file.write(final_df.schema.json())
-    final_df.coalesce(1).orderBy('Month', 'REF_DATE')\
+    final_df.coalesce(1).orderBy('Month') \
         .write.csv(OUT_PATH + 'healthcare_analysis', header='true', mode='overwrite')
     # endregion
 
